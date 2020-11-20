2505ef49d64c10bf82196665aae7efd2b58fb63a
DESKTOP-P6QN5A2\admin <1460971053@qq.com>
2020-11-20 09:32:47 +0800

    r

--- vuegit/src/views/About.vue
+++ vuegit/src/views/About.vue
@@ -5,5 +5,9 @@
     <p>fdghjklgfjgjju</p>
     <h1>你好啊伟珂</h1>
     <h2>恭喜产品经理</h2>
+    <div class="c">
+      kdsslksldlkfdlkjkjkjdsdjjfjfjf
+    </div>
+    <i>fdfgadgfd</i>
   </div>
 </template>
